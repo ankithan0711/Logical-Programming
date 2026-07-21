9510fdb6d532156388859a94d644225f66497c97
ankithan0711 <ankithan0711@gmail.com>
2026-07-21 15:55:03 +0530

    [Ankitha] Added the problem on Swapping two number

---
+++ comm/rc/numberproblems/SwapNumber.java
@@ -0,0 +1,22 @@
+package comm.rc.numberproblems;
+
+public class SwapNumber {
+	
+	public static void main(String[] args) {
+		
+		int a = 10;
+		int b= 23;
+	
+		
+		int temp =a;
+		a = b;
+		b = temp ;
+	
+		
+		System.out.println(a);
+		System.out.println(b);
+	
+		
+	}
+
+}
